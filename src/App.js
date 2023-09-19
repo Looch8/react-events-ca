39bdbb4fb8cc7fb006333b21bb1ae87e0d3f7531
Luke Wheldale <lukewheldale@gmail.com>
2023-09-19 19:19:42 +0930

    feat: app turned into class

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./App.css";
+
+// Page that lets us increase or decrease
+// How many pokemon are on screen
+// App component needs to:
+// - Keep track of how many pokemon to show
+// - iterate over a single, DRY modular pokemon component
+// - buttons to increase Pokemon count
+// - button to decrease Pokemon count
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		// Start with atleast 1 pokemon on screen
+		this.state = {
+			pokemonCount: 1,
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Pokemon page</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
